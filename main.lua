ba6cf550fa3f63556668935cae890a25fca9b246
lukehayes <lukepurplehayes@gmail.com>
2025-11-02 13:44:58 +0000

    fix: grid now drawn correctly

--- main.lua
+++ main.lua
@@ -3,12 +3,12 @@ local Game = require("F.Game")
 local grid = {}
 
 
-for x = 1, 10, 1 do
+for x = 1, 11, 1 do
 	local row = {}
-	for y = 1, 10, 1 do
+	for y = 1, 11, 1 do
 		local cell = {}
-		cell.x = x
-		cell.y = y
+		cell.x = Game.tile_size * x
+		cell.y = Game.tile_size * y
 		cell.width = Game.tile_scale * Game.tile_size
 		cell.height = Game.tile_scale * Game.tile_size
 		cell.style = 'line'
@@ -37,7 +37,7 @@ function love.draw()
 	Game.events:emit("prerender")
 
 	for _, row in ipairs(grid) do
-		for _, cell in ipairs(grid) do
+		for _, cell in ipairs(row) do
 			love.graphics.rectangle(
 				cell.style,
 				cell.x,
